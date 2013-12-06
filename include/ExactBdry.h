6085cd3280b49219c4048675010357dd4ee2666e
¨stevb¨ <stevebrust@gmail.com>
2013-12-06 15:43:12 +0100

    ExactBdry class header and definition file

--- include/ExactBdry.h
+++ include/ExactBdry.h
@@ -1,3 +1,48 @@
+<<<<<<< HEAD
+/*
+ * ExactBdry.h
+ *
+ *  Created on: Dec 5, 2013
+ *      Author: Steve Brust
+ */
+
+#ifndef EXACTBDRY_H_
+#define EXACTBDRY_H_
+
+#include <armadillo>
+using namespace arma;
+
+class ExactBdry {
+public:
+
+	mat loc;
+	mat ds;
+	mat n;
+
+	mat tau;
+	vec locx;
+	vec locy;
+	mat loco;
+	int M;
+	mat basis;
+
+	cube u;
+
+	cube x;
+	int m;
+
+	ExactBdry(mat inbound_loc, mat inbound_ds, mat inbound_n);
+
+	int npoints();
+	mat interp(cube inbound_u);
+	vec interp_tangent(cube inbound_u);
+	vec interp_normal(cube inound_u);
+	cube local_coords(cube inbound_x, int inbound_m);
+
+	virtual ~ExactBdry();
+};
+#endif /* EXACTBDRY_H_ */
+=======
 #ifndef EXACTBDRY_H
 #define EXACTBDRY_H
 
@@ -12,3 +57,4 @@ class ExactBdry {
 };
 
 #endif // EXACTBDRY_H
+>>>>>>> 57cf9c674d580d494b6bf5aba197d5d778cdd933
